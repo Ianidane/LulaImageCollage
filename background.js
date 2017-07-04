c9d09b34b9c8d8c130152a17a20c99b148988db3
ianidane <ianidane1989@gmail.com>
2017-07-04 15:29:16 -0400

    fixed small things

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('index.html', {id:"fileWin", outerBounds: {width: 1038, height: 950}}, function(win) {
+  chrome.app.window.create('index.html', {id:"fileWin", outerBounds: {width: 1200, height: 950}}, function(win) {
     win.contentWindow.launchData = launchData;
   });
 });
